1417480b9a5173d0f3169cf493efaa910ba251a4
Sergey Magidovich <mgsergio@mapswithme.com>
2015-11-03 13:26:01 +0300

    Add new style. Make it default.

---
+++ styles/mapsme-c-style.el
@@ -0,0 +1,16 @@
+(require 'google-c-style)
+
+(defconst mapsme-c-style
+  `("Google"
+    (c-offsets-alist . ((access-label . -)))))
+
+(defun mapsme-set-c-style ()
+  (interactive)
+  (make-local-variable 'c-tab-always-indent)
+  (setq c-tab-always-indent t)
+  (c-set-style "Mapsme"))
+
+(add-hook 'c-initialization-hook
+	  (lambda () (c-add-style "Mapsme" mapsme-c-style)))
+
+(provide 'mapsme-c-style)

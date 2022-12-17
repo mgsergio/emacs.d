ca6e3faa9e670abfbaa4ee3cb71a975cd9972e73
Sergei Magidovich <mgserjio@gmail.com>
2022-12-17 18:01:32 +0000

    Move more stauff to config.org.

--- init.el
+++ init.el
@@ -1,21 +1,3 @@
-(setf inhibit-startup-screen t
-      ring-bell-function 'ignore
-      visible-bell nil
-      make-backup-files nil)
-
-(set-default 'indent-tabs-mode nil)
-
-(setf help-window-select t)
-
-(if (display-graphic-p)
-    (progn
-      (scroll-bar-mode -1)
-      (tool-bar-mode -1)
-      (set-fringe-mode 15)))
-(menu-bar-mode -1)
-(global-hl-line-mode 1)
-
-
 (setf custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
@@ -34,23 +16,4 @@
 (require 'use-package-ensure)
 (setf use-package-always-ensure t)
 
-(setf debug-on-error t)
-
 (org-babel-load-file "~/.emacs.d/config.org")
-
-;; TODO: (desktop-save-mode 1)
-;; TODO: here and in mue.el trim trailing whitespaes!
-
-;; Wanted packages/features:
-;; Rename both file and buffer
-;; helm/swiper
-;; multiple-cursors
-;; mode-line?
-;; window-management
-;; hydra?
-;; grow/shrink regions?
-;; tree-sitter
-;; ace-jump ace-* ?
-;; smartparent
-;; expand-region
-;; copany-mode
